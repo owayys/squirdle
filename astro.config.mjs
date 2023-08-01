c9eee43e01b155cff09551a83d9daf36ceb2616a
owayys <owais.ahsan@hotmail.com>
2023-08-01 21:18:26 +0500

    changed project name to squirdle

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import react from "@astrojs/react";
 export default defineConfig({
     integrations: [react()],
     site: 'https://owayys.github.io',
-    base: '/weedle',
+    base: '/squirdle',
 });
